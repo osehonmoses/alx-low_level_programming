e058c301017f1b014e33d1735058d332a3b9421f
osehonmoses <osehon54@gmail.com>
2022-12-08 13:35:14 -0800

    my first c programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,16 +6,6 @@
 */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
-	printf("Size of a char: %lu byte(S)\n", (unsigned long)sizeof(a));
-	printf("Size of an int: %lu byte(S)\n", (unsigned long)sizeof(b));
-	printf("Size of a long int: %lu byte(S)\n", (unsigned long)sizeof(c));
-	printf("Size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
-	printf("Size of a float int: %lu byte(S)\n", (unsigned long)sizeof(f));
+	printf("with proper grammar, but the outcome is a piece of art")
 	return (0);
 }
