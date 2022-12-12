24d7afdcfff2331c00f24874e9413ddb0420d36a
osehonmoses <osehon54@gmail.com>
2022-12-12 03:03:02 -0800

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - write a program that print all the number of base 16
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 16; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
